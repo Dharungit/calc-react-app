f96b4aa4df9d2fd698042a86e6b9d41ccbb95a6c
Dharun <dharun123wizz@gmail.com>
2021-08-17 16:07:13 +0530

    Minor mistakes

--- src/App.js
+++ src/App.js
@@ -4,72 +4,71 @@ import { useState } from "react";
 import logo from "./logo.png";
 
 function App() {
-  
-  const CalNums = () =>{
-    const digits =[];
+  const CalNums = () => {
+    const digits = [];
 
-    for (let i=1; i<10; i++){
+    for (let i = 1; i < 10; i++) {
       digits.push(
-      <button onClick={() => updateCalc(i.toString()) } key={i} >{i}</button>
-      )
+        <button onClick={() => updateCalc(i.toString())} key={i}>
+          {i}
+        </button>
+      );
     }
     return digits;
-  }
+  };
 
   const [calc, setCalc] = useState("");
   const [result, setResult] = useState("");
 
   const opr = ["/", "*", "+", "-", "."];
 
-
-
-  const updateCalc = value => {
-    if(
-      opr.includes(value) && calc == "" ||
-      opr.includes(value) && opr.includes(calc.slice(-1))
-    ){
+  const updateCalc = (value) => {
+    if (
+      (opr.includes(value) && calc == "") ||
+      (opr.includes(value) && opr.includes(calc.slice(-1)))
+    ) {
       return;
     }
 
     setCalc(calc + value);
 
-    if(!opr.includes(value)){
+    if (!opr.includes(value)) {
       setResult(eval(calc + value).toString());
     }
-  }
+  };
 
-  const evalCalc = () =>{
-    if(calc == ""){
+  const evalCalc = () => {
+    if (calc == "") {
       return;
     }
     setCalc(eval(calc).toString());
-  }
-  
+  };
+
   const deleteLast = () => {
-    if(calc == ""){
+    if (calc == "") {
       return;
     }
     const value = calc.slice(0, -1);
 
     setCalc(value);
-  }
+  };
   const deleteAll = () => {
-    if(calc == ""){
+    if (calc == "") {
       return;
     }
     setCalc("");
-  }
+  };
 
   return (
     <div className="root">
       <div className="header">
         <img src={logo} alt="logo" width="30px" />
-        <h3>Calulator App</h3>
+        <h3>Calculator App</h3>
       </div>
       <div className="App">
         <div className="calculator">
           <div className="display">
-            {result  ? <span>({result})</span> : "" }&nbsp; {calc || "0"}
+            {result ? <span>({result})</span> : ""}&nbsp; {calc || "0"}
           </div>
 
           <div className="operators">
@@ -79,14 +78,12 @@ function App() {
             <button onClick={() => updateCalc("-")}>-</button>
             <button onClick={() => deleteLast()}>DEL</button>
             <button onClick={() => deleteAll()}>AC</button>
-
           </div>
           <div className="digits">
             {CalNums()}
             <button onClick={() => updateCalc("0")}>0</button>
             <button onClick={() => updateCalc(".")}>.</button>
             <button onClick={() => evalCalc()}>=</button>
-            
           </div>
         </div>
       </div>
